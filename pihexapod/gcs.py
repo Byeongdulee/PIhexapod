23fc67a8ab175ecbbc82a1aed9826a586f64eed2
Byeongdulee <45212115+Byeongdulee@users.noreply.github.com>
2025-03-08 16:32:56 -0600

    Update gcs.py

--- pihexapod/gcs.py
+++ pihexapod/gcs.py
@@ -254,13 +254,17 @@ class Hexapod:
         else:
             print(f"The wavetable {wavetableID} might be empty.")
     
-    def set_wav_SNAKE(self, time_per_line = 5, number_of_lines = 10, start_X0 = -2.5, X_distance=1, start_Y0 = 0, Y_step = 1, direction=1):
+    def set_wav_SNAKE(self, time_per_line = 5, start_X0 = -2.5, X_distance=1, start_Y0 = 0, start_Yf = 1, Y_step = 0.1, direction=1):
         sec4pnt = 0.001 # 1 milli-second for each pont.
         speed_up_down = 10
+        number_of_lines = int((start_Yf-start_Y0)/Y_step)+1
+        if number_of_lines%2 !=0:
+            number_of_lines+=1
+        print(f"number_of_lines is {number_of_lines}")
         totalpnts = time_per_line/sec4pnt*number_of_lines
         totalpnts4line0 = time_per_line/sec4pnt
         totalpnts4line = totalpnts4line0 + speed_up_down
-        N_round = number_of_lines/2 # the number of lines should be even number...
+        N_round = int(number_of_lines/2) # the number of lines should be even number...
         if totalpnts>self.qWMS():
             raise WAV_Exception("Too long wave.")
         wavetableID4X = 2
@@ -283,18 +287,23 @@ class Hexapod:
 #            Y_target0 = 0
 #            Y_step = 1
             # first flat 
-            cmd = f"WAV {wavetableID4Y} {isappend} LIN {totalpnts4line0/2} 0 {Y_target0:.3e} {totalpnts4line0/2} 0 0"
+            cmd = f"WAV {wavetableID4Y} {isappend} LIN {totalpnts4line0} 0 {Y_target0:.3e} {totalpnts4line0} 0 0"
             self.pidev.send_command(cmd)
             # linear increase
-            cmd = f"WAV {wavetableID4Y} & LIN {totalpnts4line0+speed_up_down*2} {Y_step:.3e} {Y_target0:.3e} {totalpnts4line0+speed_up_down*2} 0 {totalpnts4line0}"
+            cmd = f"WAV {wavetableID4Y} & LIN {speed_up_down*2} {Y_step:.3e} {Y_target0:.3e} {speed_up_down*2} 0 {speed_up_down}"
             self.pidev.send_command(cmd)
             # second flat
-            if direction>1:
+            if direction>0:
                 Y_target0 = Y_target0 + Y_step
             else:
                 Y_target0 = Y_target0 - Y_step
-            cmd = f"WAV {wavetableID4Y} & LIN {totalpnts4line0/2} 0 {Y_target0:.3e} {totalpnts4line0/2} 0 0"
+            cmd = f"WAV {wavetableID4Y} & LIN {totalpnts4line0} 0 {Y_target0:.3e} {totalpnts4line0} 0 0"
             self.pidev.send_command(cmd)
+
+                    #self.pidev.WSL(WaveGenID, waveTableID) # assign wavelet 1 to the X axis.
+        self.pidev.send_command(f"WSL {WaveGenID['X']} {wavetableID4X} {WaveGenID['Y']} {wavetableID4Y}")
+        #self.pidev.WGC(WaveGenID, number of cycles to run) # run only 1 time
+        self.pidev.send_command(f"WGC {WaveGenID['X']} 1 {WaveGenID['Y']} 1")
 
     def set_wav_x(self, totaltime=5, totaltravel=5, startposition=-2.5, pnts4speedupdown=10, direction=1):
         self.set_wav_LIN(totaltime, totaltravel, startposition, pnts4speedupdown, direction)
